7793398448f960ebd1abfdc1cd23e4b02ce7801d
casthegrinni <brunno.castegrini@hotmail.com>
2022-09-24 02:38:24 -0300

    FEAT: sem action bar

--- app/src/main/java/com/hibisco/kitsune/feature/ui/Login/View/LoginActivity.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/Login/View/LoginActivity.kt
@@ -14,8 +14,7 @@ class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         auth = Firebase.auth
     }
 
