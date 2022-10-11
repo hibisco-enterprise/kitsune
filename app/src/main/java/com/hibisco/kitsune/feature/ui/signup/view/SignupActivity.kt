3fe6ef0d9bd1cdf2c5a1cc495c37afeb14b5fa08
danzobiss <danzobiss@gmail.com>
2022-10-11 16:52:52 -0300

    Signup step 1 strongness

--- app/src/main/java/com/hibisco/kitsune/feature/ui/signup/view/SignupActivity.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/signup/view/SignupActivity.kt
@@ -3,12 +3,11 @@ package com.hibisco.kitsune.feature.ui.signup.view
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import com.hibisco.kitsune.R
-import com.hibisco.kitsune.databinding.ActivityLoginBinding
 import com.hibisco.kitsune.databinding.ActivitySignupBinding
+import java.lang.Integer.parseInt
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class SignupActivity : AppCompatActivity() {
@@ -49,14 +48,100 @@ class SignupActivity : AppCompatActivity() {
         binding.step2.visibility = View.GONE
 
         binding.btnStep1.setOnClickListener {
-            binding.signupPgb.setProgress(33, true)
-            binding.step1.visibility = View.GONE
-            binding.step2.visibility = View.VISIBLE
+            if (validateFirstStep()){
+                binding.signupPgb.setProgress(33, true)
+                binding.step1.visibility = View.GONE
+                binding.step2.visibility = View.VISIBLE
+            }
         }
         binding.btnStep2.setOnClickListener {
             binding.signupPgb.setProgress(67, true)
             binding.step2.visibility = View.GONE
             binding.step3.visibility = View.VISIBLE
+        }
+    }
+
+    private fun validateCPF(strCPFParam: String): Boolean {
+
+        var strCPF: String = strCPFParam
+
+        strCPF = strCPF.replace(".", "")
+        strCPF = strCPF.replace("-", "")
+
+        var resto: Int
+        var soma: Int = 0;
+        if (strCPF === "00000000000") return false;
+
+        for (i in 1..9) soma += parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+        resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11))  resto = 0;
+        if (resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+        soma = 0;
+        for (j in 1..10) soma += parseInt(strCPF.substring(j - 1, j)) * (12 - j);
+        resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11))  resto = 0;
+        if (resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+        return true;
+    }
+
+    private fun isPasswordStrong(password: String): Boolean {
+
+        var hasLetter: Boolean = false
+        var hasNumber: Boolean = false
+        var hasSpecialChar: Boolean = false
+
+        if (password.length < 8) {
+            binding.passwordEt.error = "Sua senha deve conter ao menos 8 caracteres."
+            return false
+        }
+        for (element in password) {
+            if (!hasLetter) if (element.toString().matches("[a-zA-Z]*".toRegex())) {
+                hasLetter = true
+            }
+
+            if (!hasNumber) if (element.toString().matches("[0-9]*".toRegex())) {
+                hasNumber = true
+            }
+
+            if (!hasSpecialChar) if (element.toString().matches("[!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*\$".toRegex())) {
+                hasSpecialChar = true
+            }
+        }
+        if (!hasLetter) {
+            binding.passwordEt.error = "Sua senha deve conter ao menos uma letra."
+            return false
+        }
+        if (!hasNumber) {
+            binding.passwordEt.error = "Sua senha deve conter ao menos um número."
+            return false
+        }
+        if (!hasSpecialChar) {
+            binding.passwordEt.error = "Sua senha deve conter ao menos um caractere especial."
+            return false
+        }
+        return true
+    }
+
+    private fun validateFirstStep(): Boolean{
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(binding.emailEt.text.toString().trim().lowercase(Locale.getDefault())).matches()){
+            //valid email
+
+            val password = binding.passwordEt.text.toString()
+            val confirmPassword = binding.confirmPasswordEt.text.toString()
+            return if(password == confirmPassword){
+                //passwords match
+
+                isPasswordStrong(password)
+            } else {
+                binding.confirmPasswordEt.error = "Senhas não correspondem!"
+                false;
+            }
+        } else {
+            binding.emailEt.error = "Email inválido!"
+            return false;
         }
     }
 }
