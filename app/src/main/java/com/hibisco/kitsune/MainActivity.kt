72562a4d3da72630ba02b9ba34cdb469c30b6edb
casthegrinni <brunno.castegrini@hotmail.com>
2022-10-24 22:58:51 -0300

    REMOVE: deletando a main activity antiga

--- app/src/main/java/com/hibisco/kitsune/MainActivity.kt
+++ app/src/main/java/com/hibisco/kitsune/MainActivity.kt
@@ -11,72 +11,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.hibisco.kitsune.databinding.ActivityMainBinding
+import com.hibisco.kitsune.databinding.ActivityMapBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        auth = Firebase.auth
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-     override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        // if(currentUser != null){  reload();  }
-    }
-
-    fun cadastro(view: View) {
-        val etEmail: EditText = findViewById(R.id.tv_email)
-        val etPassWOrd: EditText = findViewById(R.id.tv_password)
-        val email = etEmail.text.toString()
-        val password = etPassWOrd.text.toString()
-
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("TAG", "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    if (user != null) {
-                        Toast.makeText(baseContext, "Usuario criado ${user.email.toString()}", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed. $",
-                        Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
-    fun login (View: View) {
-        val etEmail: EditText = findViewById(R.id.tv_email)
-        val etPassWOrd: EditText = findViewById(R.id.tv_password)
-        val email = etEmail.text.toString()
-        val password = etPassWOrd.text.toString()
-
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("", "signInWithEmail:success")
-                    val user = auth.currentUser
-                    if (user != null) {
-                        Toast.makeText(
-                            baseContext,
-                            "Usuario logado ${user.email.toString()}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("TAG", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
 }
