f7b99b759127163ea095176ff37c9bb4043c337d
casthegrinni <brunno.castegrini@hotmail.com>
2022-10-25 09:30:28 -0300

    FIX: cor do icon

--- app/src/main/java/com/hibisco/kitsune/feature/ui/base/MainActivity.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/base/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.hibisco.kitsune.feature.ui.base
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.hibisco.kitsune.databinding.ActivityMainBinding
-import com.hibisco.kitsune.databinding.ActivityMapBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -57,6 +50,15 @@ class MainActivity : AppCompatActivity() {
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
+
+    }
+
+    override fun onBackPressed() {
+        Log.d("CDA", "onBackPressed Called")
+        val setIntent = Intent(Intent.ACTION_MAIN)
+        setIntent.addCategory(Intent.CATEGORY_HOME)
+        setIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(setIntent)
     }
 
 }
