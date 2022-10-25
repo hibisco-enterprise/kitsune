5a7300be508fd021eb8541a278d2a6deba5fd2f5
casthegrinni <brunno.castegrini@hotmail.com>
2022-10-24 22:49:55 -0300

    FEAT: fragments

--- app/src/main/java/com/hibisco/kitsune/feature/ui/faq/view/FaqFragment.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/faq/view/FaqFragment.kt
@@ -1,4 +1,19 @@
 package com.hibisco.kitsune.feature.ui.faq.view
 
-class FaqFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hibisco.kitsune.R
+
+class FaqFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_faq, container, false)
+    }
 }
--- app/src/main/java/com/hibisco/kitsune/feature/ui/map/view/MapFragment.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/map/view/MapFragment.kt
@@ -8,7 +8,12 @@ import android.view.ViewGroup
 import com.hibisco.kitsune.R
 
 class MapFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_map, container, false)
+    }
 }
