cda8b200bd50a36eea26e027ab7ce9f3c59bb0e5
casthegrinni <brunno.castegrini@hotmail.com>
2022-11-01 16:08:29 -0300

    FEAT: finish()

--- app/src/main/java/com/hibisco/kitsune/feature/ui/map/view/ConfirmDonationActivity.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/map/view/ConfirmDonationActivity.kt
@@ -3,10 +3,23 @@ package com.hibisco.kitsune.feature.ui.map.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hibisco.kitsune.R
+import com.hibisco.kitsune.databinding.ActivityConfirmDonationBinding
+import com.hibisco.kitsune.databinding.ActivityLoginBinding
 
 class ConfirmDonationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConfirmDonationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirm_donation)
+        binding = ActivityConfirmDonationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setActions()
+    }
+
+    fun setActions() {
+        binding.btnX.setOnClickListener {
+            finish()
+        }
     }
 }
