5591bf7a31b5ed0be82f757df3cc3578bb81c6b0
danzobiss <danzobiss@gmail.com>
2022-12-12 15:25:12 -0300

    feat: Toggle das perguntas no FAQ

--- app/src/main/java/com/hibisco/kitsune/feature/ui/faq/view/FaqFragment.kt
+++ app/src/main/java/com/hibisco/kitsune/feature/ui/faq/view/FaqFragment.kt
@@ -5,15 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hibisco.kitsune.R
+import com.hibisco.kitsune.databinding.ActivityFaqBinding
 
 class FaqFragment : Fragment() {
+
+    private var _binding: ActivityFaqBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = ActivityFaqBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_faq, container, false)
+
+        binding.question1.setOnClickListener {
+            toggleAnswer(binding.answer1)
+        }
+        binding.question2.setOnClickListener {
+            toggleAnswer(binding.answer2)
+        }
+        binding.question3.setOnClickListener {
+            toggleAnswer(binding.answer3)
+        }
+        binding.question4.setOnClickListener {
+            toggleAnswer(binding.answer4)
+        }
+        binding.question5.setOnClickListener {
+            toggleAnswer(binding.answer5)
+        }
+        binding.question6.setOnClickListener {
+            toggleAnswer(binding.answer6)
+        }
+        binding.question7.setOnClickListener {
+            toggleAnswer(binding.answer7)
+        }
+        binding.question8.setOnClickListener {
+            toggleAnswer(binding.answer8)
+        }
+        binding.question9.setOnClickListener {
+            toggleAnswer(binding.answer9)
+        }
+        binding.question10.setOnClickListener {
+            toggleAnswer(binding.answer10)
+        }
+
+        return binding.root
+    }
+
+    private fun toggleAnswer(answer: View){
+        answer.visibility = if (answer.visibility == View.GONE) View.VISIBLE else View.GONE
     }
 }
